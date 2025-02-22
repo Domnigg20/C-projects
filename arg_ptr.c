1e5b6f824db31327bd6d8055aa7a2dfa97979a6d
Peter Ariyibi <100387265+Domnigg20@users.noreply.github.com>
2025-02-22 21:08:34 +0000

    Update arg_ptr.c

--- arg_ptr.c
+++ arg_ptr.c
@@ -1,3 +1,8 @@
+
+//Design a function that takes a string pointer as an argument and returns a pointer
+//to the first space character in the string on or after the pointed-to position. Have
+//it return a null pointer if it finds no spaces.
+
 #include<stdio.h>
 
 char* ptr_space (const char* str) {
